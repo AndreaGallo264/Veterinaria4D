e7b73a200ee69798706b964e3ddb13272ebafaee
Andrea Gallo <andreagallo264@gmail.com>
2020-05-20 20:19:55 -0300

    "Login comenzado"

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { Form } from 'react-bootstrap';
 
 const Login = () => {
-    return (  );
+    return ( 
+        <Form>
+            
+        </Form>
+    );
 }
  
 export default Login;
